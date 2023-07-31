75269f99c26a246b9152cdb02163edfe74c1c61e
PrashastVats1 <prashastvats@gmail.com>
2023-07-31 15:03:47 +0530

    Assignment 19, Part 1 & 2

---
+++ ConAppDelegatesAssignment19_Part1/ConAppDelegatesAssignment19_Part1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDelegatesAssignment19_Part1
+{
+    delegate int ArithmeticOp(int num1, int num2);
+    class Program
+    {
+        static int Add(int num1, int num2) => num1 + num2;
+        static int Subtract(int num1, int num2) => num1 - num2;
+        static int Multiply(int num1, int num2) => num1 * num2;
+        static int Divide(int num1, int num2) => num2 != 0 ? num2 / num1 : throw new ArgumentException("Cannot divide by 0");
+        static void Main(string[] args)
+        {
+            ArithmeticOp add = Add;
+            ArithmeticOp subtract = Subtract;
+            ArithmeticOp multiply = Multiply;
+            ArithmeticOp divide = Divide;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter number 1");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter number 2");
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Choose an arithmetic expression to be executed");
+                    Console.WriteLine("1. Addition");
+                    Console.WriteLine("2. Subtraction");
+                    Console.WriteLine("3. Multiplication");
+                    Console.WriteLine("4. Division");
+                    Console.WriteLine("5. Exit");
+                    int choice = int.Parse(Console.ReadLine());
+                    int result = 0;
+                    switch (choice)
+                    {
+                        case 1:
+                            result = add(num1, num2);
+                            break;
+                        case 2:
+                            result = subtract(num1, num2);
+                            break;
+                        case 3:
+                            result = multiply(num1, num2);
+                            break;
+                        case 4:
+                            result = divide(num1, num2);
+                            break;
+                        case 5:
+                            Console.WriteLine("Exiting the program. Press any key to exit");
+                            Console.ReadKey();
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again");
+                            continue;
+                    }
+                    Console.WriteLine("Result: " + result);
+                }
+                catch (Exception ex) 
+                { Console.WriteLine("Error: " + ex.Message); }
+            }
+        }
+    }
+}
---
+++ ConAppDelegatesAssignment19_Part2/ConAppDelegatesAssignment19_Part2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppDelegatesAssignment19_Part2
+{
+    delegate T Operation<T>(T a, T b);
+    class Program
+    {
+        static T Add<T>(T a, T b)=>(dynamic)a + (dynamic) b;
+        static T Subtract<T>(T a, T b) => (dynamic)a - (dynamic)b;
+        static T Multiply<T>(T a, T b) => (dynamic)a * (dynamic)b;
+        static T Divide<T>(T a, T b) => (dynamic)a / (dynamic)b;
+        static dynamic ReadInput()
+        {
+            while (true)
+            {
+                try
+                {
+                    return Convert.ChangeType(Console.ReadLine(), typeof(double));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input, Please try again");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter value 1");
+                dynamic num1 = ReadInput();
+                Console.WriteLine("Enter value 2");
+                dynamic num2 = ReadInput();
+                Console.WriteLine("Choose an operation");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+                int choice = int.Parse(Console.ReadLine());
+                Operation<dynamic> operation = null;
+                switch (choice)
+                {
+                    case 1:
+                        operation = Add;
+                        break;
+                    case 2:
+                        operation = Subtract;
+                        break;
+                    case 3:
+                        operation = Multiply;
+                        break;
+                    case 4:
+                        operation = Divide;
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting the program, press any key");
+                        Console.ReadKey();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, Try again");
+                        continue;
+                }
+                if(operation != null)
+                {
+                    try
+                    {
+                        dynamic result = operation(num1, num2);
+                        Console.WriteLine("Result: " + result);
+                    }
+                    catch(Exception ex) 
+                    {
+                        Console.WriteLine("Error: "+ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
